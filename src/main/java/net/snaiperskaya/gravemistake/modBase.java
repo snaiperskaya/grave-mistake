57d0456a9be71afb0fa35dca796e70a6ffb1c5df
snaiperskaya <aquarist1@gmail.com>
2020-11-16 19:06:51 -0500

    Initial Config

--- src/main/java/net/snaiperskaya/gravemistake/modBase.java
+++ src/main/java/net/snaiperskaya/gravemistake/modBase.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package net.snaiperskaya.gravemistake;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class modBase implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
